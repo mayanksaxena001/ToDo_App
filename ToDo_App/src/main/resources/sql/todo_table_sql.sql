37437fd86ca51a68607b2c3baf98fe16adb6f95b
mayanksaxena001 <mayanksaxena001@gmail.com>
2016-12-19 23:19:35 +0530

    updated postgres

--- ToDo_App/src/main/resources/sql/todo_table_sql.sql
+++ ToDo_App/src/main/resources/sql/todo_table_sql.sql
@@ -1,8 +1,8 @@
---DROP DATABASE TODO;
---CREATE DATABASE TODO;
---USE TODO;
-drop table postgres.todo_items;
-create table postgres.todo_items(
+DROP DATABASE TODO;
+CREATE DATABASE TODO;
+USE TODO;
+drop table TODO.todo_items;
+create table TODO.todo_items(
 id int NOT NULL ,
 text varchar(255),
 created_date date,
