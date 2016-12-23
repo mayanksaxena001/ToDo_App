812104067d55708085e361fb6d196390b9fc3d57
Mayank Saxena <mayanksaxena001@gmail.com>
2016-12-23 04:54:08 -0500

    Update todo_table_sql.sql

--- ToDo_App/src/main/resources/sql/todo_table_sql.sql
+++ ToDo_App/src/main/resources/sql/todo_table_sql.sql
@@ -2,12 +2,13 @@
 --CREATE DATABASE TODO;
 --USE TODO;
 CREATE USER todo_app WITH PASSWORD 'todo_app';
-CREATE SCHEMA IF NOT EXISTS postgres ;
-GRANT ALL ON ALL postgres TO todo_app ;
-GRANT ALL ON ALL TABLES IN SCHEMA postgres TO todo_app ;
-SELECT * FROM pg_users;
-drop table IF EXISTS postgres.todo_items;
-create table postgres.todo_items(
+CREATE SCHEMA IF NOT EXISTS postgres_1 ;
+GRANT ALL ON ALL postgres_1 TO todo_app ;
+GRANT ALL ON ALL TABLES IN SCHEMA postgres_1 TO todo_app ;
+SELECT usename FROM pg_users;
+ALTER USER todo_app SET search_path=postgres_1;
+drop table IF EXISTS postgres_1.todo_items;
+create table postgres_1.todo_items(
 id int NOT NULL ,
 text varchar(255),
 created_date date,
