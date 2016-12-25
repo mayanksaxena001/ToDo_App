1a0dc416434fcdf95ad58f3d631fcb60f9b90a0c
mayanksaxena001 <mayanksaxena001@gmail.com>
2016-12-25 18:47:10 +0530

    Database updated

--- ToDo_App/src/main/java/com/database/DatabaseInit.java
+++ ToDo_App/src/main/java/com/database/DatabaseInit.java
@@ -2,7 +2,6 @@ package com.database;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.net.URI;
 import java.net.URISyntaxException;
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -24,69 +23,39 @@ public class DatabaseInit {
 	DataSource dataSource;
 	
 	@PostConstruct
-	public void init(){
-//		URI dbUri=null;
-//		try {
-//			 dbUri = new URI(System.getenv("DATABASE_URL"));
-//		} catch (URISyntaxException e1) {
-//			e1.printStackTrace();
-//		}
-//		String db=dbUri.getPath().substring(1);
-//		String schema="connect "+"postgres";
-//			String 	sql_="create table "+"postgres"+".todo_items("+
-//				"id int NOT NULL ,"+
-//				"text varchar(255),"+
-//				"created_date date,"+
-//				"due_date date,"+
-//				"primary key(id)"+
-//				");";
-//		System.out.println("Executing sql script...");
-//		System.out.println(sql_);
-//		try {
-//			Connection con=dataSource.getConnection();
-//			Statement statement = con.createStatement();
-//			statement.executeUpdate(schema);
-//			statement.executeUpdate(sql_);
-//			
-//		} catch (SQLException e) {
-//			e.printStackTrace();
-//			return;
-//		}
-		try (Connection connection = dataSource.getConnection();
-                Statement statement = connection.createStatement()) {
-               for(String sql: retrieveSqls()) {
-                   statement.executeUpdate(sql);
-                   
-               }
-           } catch (SQLException | IOException e) {
-               System.out.println(e.getMessage());
-               return ;
-           }
+	public void init() {
+
 		System.out.println("Executing sql script...");
 
-		try (Connection connection = dataSource.getConnection();
-               Statement statement = connection.createStatement()) {
-		statement.executeUpdate("SELECT usename FROM "+getConnection()+".pg_users;");
-              for(String sql: retrieveSqls()) {
-		  System.out.println(sql);
-                  statement.executeUpdate(sql);  
-              }
-          } catch (Exception e) {
-              System.out.println(e.getMessage());
-              return ;
-          }
+		try (Connection connection = getConnection(); Statement statement = connection.createStatement()) {
+			// statement.executeUpdate("SELECT usename FROM " + getConnection()
+			// + ".pg_users;");
+			for (String sql : retrieveSqls()) {
+				System.out.println(sql);
+				statement.executeUpdate(sql);
+			}
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+			return;
+		}
 	}
 	
-	private static String getConnection() throws URISyntaxException, SQLException {
+	private static Connection getConnection() throws URISyntaxException, SQLException {
+	    String dbUrl = System.getenv("JDBC_DATABASE_URL");
+	    System.out.println(dbUrl);
+	    return DriverManager.getConnection(dbUrl);
+	}
+	
+	/*private static String getConnection() throws URISyntaxException, SQLException {
 	    URI dbUri = new URI(System.getenv("DATABASE_URL"));
 
 	    String username = dbUri.getUserInfo().split(":")[0];
 	    String password = dbUri.getUserInfo().split(":")[1];
 	    String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath();
-	    System.out.println( dbUri.getPath());
+	    System.out.println(dbUri.getPath().substring(1));
 
 	    return dbUri.getPath().substring(1);
-	}
+	}*/
 	
 	 private static Connection getMasterConnection() throws SQLException {
 	        return DriverManager.getConnection("jdbc:mysql://localhost:3306/mysql","root","root");
